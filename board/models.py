dffdbb1ddf3c99b3e057f5595e63419b364029b8
Jaewon Kim <sanvit@sanvit.me>
2020-09-20 18:53:13 +0900

    fix

--- board/models.py
+++ board/models.py
@@ -3,22 +3,27 @@ from user.models import User
 from proj import settings
 
 # Create your models here.
+
+
 class Board(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     body = models.TextField()
-    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, related_name="post"), 
+    writer = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     # writer = models.ForeignKey('user.User', on_delete = models.CASCADE)
 
-    def __str__(self): 
+    def __str__(self):
         return self.title
-        #제목으로 표시되게
+        # 제목으로 표시되게
+
 
 class Comment(models.Model):
     objects = models.Manager()
-    board = models.ForeignKey('Board', on_delete=models.CASCADE, related_name='comments')
+    board = models.ForeignKey(
+        'Board', on_delete=models.CASCADE, related_name='comments')
     comment_user = models.CharField(max_length=20)
     comment_body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -27,7 +32,6 @@ class Comment(models.Model):
     #approved_comment = models.BooleanField(default=False)
     # class Meta:
     # ordering=['id'] # 정렬기준
-
 
     # def approve(self):
     #     self.approved_comment = True
